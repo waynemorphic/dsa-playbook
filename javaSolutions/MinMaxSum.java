6af21c1d57584b8a625fe79cca14fea01aa193e1
waynemorphic <wayneklyde@gmail.com>
2023-01-03 16:55:05 +0300

    solution to MinMaxSum problem and moved java solutions to own package

---
+++ javaSolutions/MinMaxSum.java
@@ -0,0 +1,28 @@
+package javaSolutions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class MinMaxSum{
+
+    public static void solution(List<Integer> arr){
+        long sum = 0L;
+      
+        for (int x : arr){
+            sum += x;
+        }
+            System.out.printf("%d %d",sum - Collections.max(arr), sum - Collections.min(arr));
+                
+    }
+    public static void main(String[] args){
+        List<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        MinMaxSum.solution(nums);
+    }
+}
