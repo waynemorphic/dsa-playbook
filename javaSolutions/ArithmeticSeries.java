31afb40c3ab7705dd3e92b59f4c138f2ac188aac
waynemorphic <wayneklyde@gmail.com>
2023-01-09 13:11:20 +0300

    ArithmeticSeries solution in java

--- javaSolutions/ArithmeticSeries.java
+++ javaSolutions/ArithmeticSeries.java
@@ -6,10 +6,17 @@ public class ArithmeticSeries{
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        for(int i = 0;i < t; i++){
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
+            
+            for (int k = 0; k < n; k++){
+                int j = a + (int)(Math.pow(2, k) * b ) + (int)(Math.pow(2, k) - 1) * b; 
+                System.out.print(j + " ");
+            }
+            System.out.println();
+            
         }
         in.close();
     }
