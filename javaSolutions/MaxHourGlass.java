3105552276641bf34fd2a6171e3489f547aac482
waynemorphic <wayneklyde@gmail.com>
2023-01-19 15:00:56 +0300

    debug

--- javaSolutions/MaxHourGlass.java
+++ javaSolutions/MaxHourGlass.java
@@ -18,7 +18,7 @@ public class MaxHourGlass {
             for (int j = 0; j <= C - 3; j++){
                 int sum = (arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2)) + 
                 (arr.get(i + 1).get(j + 1)) + 
-                (arr.get(i + 2).get(j) + arr.get(1 + 2).get(j + 1) + arr.get(i + 2).get(j + 2));
+                (arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2));
 
                 maxSum = Math.max(maxSum, sum);
             }
