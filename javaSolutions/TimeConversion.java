8d26101beb1b4f1d81cec9fcedad557b1e0032ec
waynemorphic <wayneklyde@gmail.com>
2023-01-30 22:23:41 +0300

    alternative time conversion method

--- javaSolutions/TimeConversion.java
+++ javaSolutions/TimeConversion.java
@@ -28,6 +28,36 @@ public class TimeConversion {
         
     }
 
+    // Method two
+    public static String timeConversion(String s) {
+        // Write your code here
+        String pm = "PM";
+        String am = "AM";
+        String hh = s.substring(0,2);
+        String mm = s.substring(3,5);
+        String ss = s.substring(6, 8);
+        String timeFormat = s.substring(8, s.length());
+        int time = Integer.parseInt(hh);
+        String military = "";
+        
+        if (timeFormat.equals(pm) && hh.equals("12")){
+            military = s.substring(0,8);
+        }
+        else if (timeFormat.equals(pm)){
+            time += 12;                       
+            military = String.valueOf(time)+":"+mm+":"+ss;
+        }
+        else if (timeFormat.equals(am) && hh.equals("12")){
+            // time = Integer.parseInt(hh) - 12;
+            // System.out.println(time); 
+            military = String.format("00"+":"+mm+":"+ss);
+        }
+        else {
+            military = hh+":"+mm+":"+ss;
+        }
+        return military;
+    }
+
     public static void main(String[] args){
         String input = "07:18:01PM";
         TimeConversion.solution(input);
